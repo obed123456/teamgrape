e28e68b9045b44e786e31e2d44f6d410128d3ef7
jessejonsson <32056772+jessejonsson@users.noreply.github.com>
2018-01-17 15:32:28 +0100

    fixed code modal

--- JS/codeGenerator.js
+++ JS/codeGenerator.js
@@ -2,8 +2,8 @@ function check() {
     let code = document.getElementById('codeInput').value;
   /*function to check userid & password
                 the following code checkes whether the entered userid and password are matching*/
-      if (code == "join"){
-         window.open("https://www.google.se/");
+      if (code == "123"){
+         window.open("joinplayer.html", "_self");
       } else {
         console.log("false");
         alert("false");
