c7aa07d934518e16c0cb34dda239479a4fa4618a
obed123456 <31744395+obed123456@users.noreply.github.com>
2018-02-22 11:00:14 +0100

    add starttime

--- node-api/rest.js
+++ node-api/rest.js
@@ -119,9 +119,9 @@ router.delete("/deletemarker/:id",function(req,res){
 
 //Start match 
 
-router.post("/startmatch/:user_name/:match_code", function(req,res){
-    var query = "INSERT INTO ??(??,??) VALUES (?,?)";
-    var table = ["match","uname","matchCode",req.params.user_name, req.params.match_code];
+router.post("/startmatch/:user_name/:match_code/:start_time", function(req,res){
+    var query = "INSERT INTO ??(??,??,??) VALUES (?,?,?)";
+    var table = ["match","uname","matchCode", "startTime",req.params.user_name, req.params.match_code, req.params.start_time];
     query = mysql.format(query,table);
     connection.query(query,function(err,rows){
         if(err) {
